16d3448d3e02449f6d2ae1f7352940f12f6671e8
dazzleb <dazzledazzleb@gmail.com>
2023-02-14 18:44:43 +0900

    2week 시작

---
+++ 2weekStoryBoard/2weekStoryBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2weekStoryBoard
+//
+//  Created by 시혁 on 2023/02/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
