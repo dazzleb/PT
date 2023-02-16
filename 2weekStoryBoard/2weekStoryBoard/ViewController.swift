a9989434d49c7bbc4274e673303fd998e03a132e
dazzleb <dazzledazzleb@gmail.com>
2023-02-16 18:57:31 +0900

    ScrollView 연습

--- 2weekStoryBoard/2weekStoryBoard/ViewController.swift
+++ 2weekStoryBoard/2weekStoryBoard/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var isLeadingFoucused = true
+    @IBOutlet weak var leading: NSLayoutConstraint!
+    @IBOutlet weak var trailing: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func button(_ sender: UIButton) {
+        print(#fileID, #function, #line, "- button" )
+        self.view.layoutIfNeeded()
+        UIView.animate(withDuration: 0.5){
+            if self.isLeadingFoucused {
+                self.trailing.priority = UILayoutPriority.defaultHigh
+                self.leading.priority = UILayoutPriority.defaultLow
+            }else {
+                self.leading.priority = UILayoutPriority.defaultHigh
+                self.trailing.priority = UILayoutPriority.defaultLow
+            }
+            self.isLeadingFoucused.toggle()
+            self.view.layoutIfNeeded()
+        }
+    }
+    
 }
 
